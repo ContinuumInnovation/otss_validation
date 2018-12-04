9cb3e8018bd003926098969bf23c77b8288f2914
Adam Casey <adamjcasey@gmail.com>
2018-12-04 10:36:35 -0500

    Lint fail

--- src/main.cc
+++ src/main.cc
@@ -35,9 +35,7 @@ Created by Adam Casey 2016
 
 #include <iostream>
 
-#include "main.h"
-
-int main(void)
+int main(void)// This comment will cause lint to fail
 {
     std::cout << "Validating Travis CI" << std::endl;
 
